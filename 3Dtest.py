80830892cb27fee3167356509e2c10260e39e520
marimo <marimo@users.noreply.github.com>
2020-04-22 21:22:26 +0900

    :+1: 点群表示機能、点群保存機能追加

--- 3Dtest.py
+++ 3Dtest.py
@@ -1,7 +1,14 @@
 
 import open3d as o3d
 
-# 3DメッシュをGUIで表示
+# 3Dメッシュで表示
 mesh = o3d.io.read_triangle_mesh("Bunny.ply")
 o3d.visualization.draw_geometries([mesh])
 print(mesh)
+
+# 点群で表示
+pointcloud = o3d.io.read_point_cloud("Bunny.ply")
+o3d.visualization.draw_geometries([pointcloud])
+
+# pcdデータをファイルに保存
+o3d.io.write_point_cloud("Bunny.pcd", pointcloud)
