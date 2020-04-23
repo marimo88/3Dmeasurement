560db7e9adcbb39180f60d527907eac5928d008d
marimo <marimo@users.noreply.github.com>
2020-04-23 21:58:19 +0900

    :+1:  AxisAlignedBoundingBox()のテスト

--- 3Dtest.py
+++ 3Dtest.py
@@ -1,14 +1,10 @@
 
 import open3d as o3d
 
-# 3Dメッシュで表示
+# メッシュをバインディングボックスで囲う
+bbox = o3d.geometry.AxisAlignedBoundingBox(min_bound=(-30, 0, -10),
+                                               max_bound=(10, 20, 10))
 mesh = o3d.io.read_triangle_mesh("Bunny.ply")
-o3d.visualization.draw_geometries([mesh])
+aabox = mesh.get_axis_aligned_bounding_box()
 print(mesh)
-
-# 点群で表示
-pointcloud = o3d.io.read_point_cloud("Bunny.ply")
-o3d.visualization.draw_geometries([pointcloud])
-
-# pcdデータをファイルに保存
-o3d.io.write_point_cloud("Bunny.pcd", pointcloud)
+o3d.visualization.draw_geometries([mesh,aabox])
