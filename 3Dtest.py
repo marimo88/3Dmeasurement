ccf8be4c2ca19ea4a36201f9ecdf842b08e149f7
marimo <marimo@users.noreply.github.com>
2020-04-26 11:06:59 +0900

    :+1: 一部のメッシュデータだけ抽出

--- 3Dtest.py
+++ 3Dtest.py
@@ -1,10 +1,18 @@
 
+import copy
 import open3d as o3d
+import numpy as np
 
-# メッシュをバインディングボックスで囲う
-bbox = o3d.geometry.AxisAlignedBoundingBox(min_bound=(-30, 0, -10),
-                                               max_bound=(10, 20, 10))
 mesh = o3d.io.read_triangle_mesh("Bunny.ply")
-aabox = mesh.get_axis_aligned_bounding_box()
-print(mesh)
-o3d.visualization.draw_geometries([mesh,aabox])
+mesh.compute_vertex_normals()
+mesh1 = copy.deepcopy(mesh)
+
+# データの前半のメッシュデータだけを抽出
+mesh1.triangles = o3d.utility.Vector3iVector(
+        np.asarray(mesh1.triangles)[:len(mesh1.triangles) // 2, :])
+mesh1.triangle_normals = o3d.utility.Vector3dVector(
+        np.asarray(mesh1.triangle_normals)[:len(mesh1.triangle_normals) //  2, :])
+
+# 加工後データ表示
+print(mesh1)
+o3d.visualization.draw_geometries([mesh1])
